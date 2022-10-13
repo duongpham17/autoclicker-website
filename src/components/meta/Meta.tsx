82e76802ba0db13d4b1ef32b0f41d5528f62570e
duongpham17 <duongpham17@icloud.com>
2022-10-13 19:35:16 +0100

    seo

--- src/components/meta/Meta.tsx
+++ src/components/meta/Meta.tsx
@@ -14,11 +14,11 @@ export const Meta = ({description = defaultDescription }: Props) =>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content={description} />
-        <meta name="og:title" content="Autoclicker" key="og:title" />
-        <meta property="og:url" content="" key="og:url"/>
+        <meta name="og:title" content="autoclickers.co.uk" key="og:title" />
         <meta property="og:type" content="website" key="og:type"/>
-        <meta property="og:image" content="images/favicon.ico" />
-        <meta property="og:description" content="Customise your own autoclicker and share with others. Color detection, clicking, movements, typing and keyboard events." />
+        <meta property="og:image" content="images/logo.png" />
+        <meta property="og:description" content="Customise your own autoclicker and share with others. Color detection, mouse clicking, mouse movements, typing and keyboard events." />
+        <meta name="keywords" content="mouse clicking, mouse movements, desktop, mac, github, free, robot, automation, scripting, scripts, script, friends, share with friends, color detection, keyboard events, auto type words, auto typing, pixel color, detect screen pixel color, fast, easy" />
         <link rel="shortcut icon" href="images/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon.ico" />
 	    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon.ico" />
