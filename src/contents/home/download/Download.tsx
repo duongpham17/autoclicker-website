a1d8ceb540cd8ce40bcbd2aecdc0367ee12640ec
duongpham17 <duongpham17@icloud.com>
2022-11-01 01:47:59 +0000

    windows

--- src/contents/home/download/Download.tsx
+++ src/contents/home/download/Download.tsx
@@ -10,50 +10,47 @@ const Download = () => {
     window: 0,
   });
 
-  const onDownload = async (name: "mac" | "window") => {
+  const mac_dmg_url = "https://ipfs.io/ipfs/bafybeibiodppvbacfcsze5t2mibipuzhm4tnopsqu22g3huf6t2qlkddiy";
+  const window_exe_url = "https://ipfs.io/ipfs/bafybeigxtdhkwybxugozbddrie2gwtywpw726nmdq6wriearvbzgr62psi";
+
+  const onDownload = async (name: "mac" | "window", url: string) => {
     if(name === "mac" && progress.mac) return;
     if(name === "window" && progress.window) return;
-
-    const mac_dmg_url = "https://ipfs.io/ipfs/bafybeibiodppvbacfcsze5t2mibipuzhm4tnopsqu22g3huf6t2qlkddiy";
-    const window_exe_url = "";
-
-    const url = name === "mac" ? mac_dmg_url : window_exe_url;
 
     const file = await axios({
       url: url,
       method: "GET",
       responseType: "blob", // important
       onDownloadProgress: (progressEvent) => {
-        console.log(progressEvent.progress);
-          if(name === "mac") setProgress(state => ({...state, mac: Number(progressEvent.progress)}));
-          if(name === "window") setProgress(state => ({...state, window: Number(progressEvent.progress)}));
+        if(name === "window") setProgress(state => ({...state, window: Number(progressEvent.progress)}));
+        if(name === "mac") setProgress(state => ({...state, mac: Number(progressEvent.progress)}));
       }
     });
     const href = URL.createObjectURL(file.data);
     const link = document.createElement('a');
     link.href = href;
     if(name === "mac") link.setAttribute('download', 'Autoclickers.dmg'); //or any other extension
-    if(name === "window") link.setAttribute('download', 'Autoclickers.dmg'); //or any other extension
+    if(name === "window") link.setAttribute('download', 'Autoclickers.exe'); //or any other extension
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
     URL.revokeObjectURL(href);
-    if(name === "mac")setProgress(state => ({...state, mac: 0}))
-    if(name === "window")setProgress(state => ({...state, mac: 0}))
+    if(name === "mac") setProgress(state => ({...state, mac: 0}))
+    if(name === "window") setProgress(state => ({...state, mac: 0}));
   };
 
   return (
     <div className={styles.container}>
 
-      <button onClick={() => onDownload("mac")}>
+      <button onClick={() => onDownload("mac", mac_dmg_url)} className={`${progress.mac > 0 && styles.started}`} >
         <span><BsApple/></span>
-        <span>Download for Mac</span>
+        <span>Mac</span>
         <progress value={progress.mac.toFixed(2)} max="1"/> 
       </button>
 
-      <button>
+      <button onClick={() => onDownload("window", window_exe_url)} className={`${progress.window > 0 && styles.started}`}>
         <span><BsWindows/></span>
-        <span>Not supported yet</span>
+        <span>Windows</span>
         <progress value={progress.window.toFixed(2)} max="1"/>
       </button>
 
