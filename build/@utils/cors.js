7da3135027865e802dcae35476a35fefcb4ece14
duongpham17 <duongpham17@icloud.com>
2025-11-05 16:28:50 +0000

    first commit;

--- build/@utils/cors.js
+++ build/@utils/cors.js
@@ -4,25 +4,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsPublic = exports.corsPrivate = void 0;
+// cors.ts (backend CORS middleware setup)
 const cors_1 = __importDefault(require("cors"));
-const website_url = [];
-const production_frontend_url = [];
-const development_frontend_url = ["http://localhost:3000", "http://localhost:3001"];
-// whitelisted website only
-exports.corsPrivate = (() => {
-    const productionURL = website_url;
-    const developmentURL = development_frontend_url;
-    const whitelist = process.env.NODE_ENV === "development" ? developmentURL : productionURL;
-    return (0, cors_1.default)({
-        origin: whitelist,
-        methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept'],
-    });
-})();
-// Public use only
-exports.corsPublic = (() => {
-    return (0, cors_1.default)({
-        origin: "*",
-        methods: ['GET'],
-    });
-})();
+const production_url = [
+    "https://autoclicker-f60h.onrender.com"
+];
+const development_url = [
+    'http://localhost:3000'
+];
+const whitelist = process.env.NODE_ENV === 'development' ? development_url : production_url;
+exports.corsPrivate = (0, cors_1.default)({
+    origin: (origin, callback) => {
+        if (!origin || whitelist.includes(origin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'device-remember-token',
+        'Access-Control-Allow-Origin',
+        'Origin',
+        'Accept',
+    ],
+});
+// simple public CORS for GET requests
+exports.corsPublic = (0, cors_1.default)({
+    origin: '*',
+    methods: ['GET'],
+});
--- src/@utils/cors.ts
+++ src/@utils/cors.ts
@@ -1,31 +1,39 @@
+// cors.ts (backend CORS middleware setup)
 import cors from 'cors';
 
-const website_url: string[] = []
+const production_url: string[] = [
+  "https://autoclicker-f60h.onrender.com"
+];
 
-const production_frontend_url: string[] = []
+const development_url: string[] = [
+    'http://localhost:3000'
+];
 
-const development_frontend_url: string[] = ["http://localhost:3000", "http://localhost:3001"]
+const whitelist: string[] =
+  process.env.NODE_ENV === 'development' ? development_url : production_url;
 
-// whitelisted website only
-export const corsPrivate = (() => {
+export const corsPrivate = cors({
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+    'device-remember-token',
+    'Access-Control-Allow-Origin',
+    'Origin',
+    'Accept',
+  ],
+});
 
-    const productionURL: string[] = website_url;
-
-    const developmentURL: string[] = development_frontend_url;
-
-    const whitelist: string[] = process.env.NODE_ENV === "development" ? developmentURL : productionURL;
-
-    return cors({
-        origin: whitelist,
-        methods: ['GET','POST','DELETE','PUT','PATCH'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept'],
-    });
-})();
-
-// Public use only
-export const corsPublic = (() => {
-    return cors({
-        origin: "*",
-        methods: ['GET'],
-    });
-})();
+// simple public CORS for GET requests
+export const corsPublic = cors({
+  origin: '*',
+  methods: ['GET'],
+});
