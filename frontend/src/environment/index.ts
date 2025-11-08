129cb4ebb74761f567a2844faf53db6fd41b0520
duongpham17 <duongpham17@icloud.com>
2025-11-08 22:44:25 +0000

    first commit

--- frontend/src/environment/index.ts
+++ frontend/src/environment/index.ts
@@ -15,6 +15,6 @@ export const base_url_frontend = {
 };
 
 export const stripe_publishable =  {
-    production: "pk_test_51SNbx7DcBcFoXM8E5SunYDydxfMd0TMLjoHD6zcOOre3EEEshAm06GOje9lP8PIpuu3gbv6lfG2ziJCNRaOZMawn00QTQCjJru",
+    production: "pk_live_51SNbx1Dx44EO0kMFFC8Jb5NgoGbBbuLqFr0Cdq4A1dAyMDGlIJQQfZbsrvP4fw0bs7QPVFqIuzs3u6GltRtJnu7k00QcwgraIL",
     development: "pk_test_51SNbx7DcBcFoXM8E5SunYDydxfMd0TMLjoHD6zcOOre3EEEshAm06GOje9lP8PIpuu3gbv6lfG2ziJCNRaOZMawn00QTQCjJru"
 };
